feeb25252fde7c67108d7c5833a6342ca6ba8c8a
NIX\bence.balogh <bence.balogh@nixs.com>
2025-07-14 15:24:57 +0200

    pw test

---
+++ pw/tests/example.spec.ts
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/test';
+
+test('get started link', async ({ page }) => {
+  await page.goto(process.env.baseURL || "http://localhost:5173");
+
+  await page.locator("div.Tatooine").click();
+  await expect(page.locator(".SWCard-selected.Tatooine .title")).toHaveText("Tatooine");
+});
