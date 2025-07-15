0f2b4b954359f0aab12ec8d5364ab40e52c33eb8
NIX\bence.balogh <bence.balogh@nixs.com>
2025-07-15 14:37:03 +0200

    baseurl change

--- SWfrontend/src/consts/swapiConsts.ts
+++ SWfrontend/src/consts/swapiConsts.ts
@@ -1,5 +1,6 @@
 
-export const BASEURL: string = "http://localhost:3000/";
+// Ill need to set up proper .env files for these. Will have a local and a deploy environment.
+export const BASEURL: string = "https://sw-ej2a.onrender.com/";
 export const PLANETS: string = "planets";
 export const PEOPLE: string = "people";
 export const FILMS: string = "films";
