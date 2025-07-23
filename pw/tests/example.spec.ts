c5d5cf20b691f8c2affe76647056b688b74c8242
NIX\bence.balogh <bence.balogh@nixs.com>
2025-07-23 12:33:46 +0200

    logging url for debugging

--- pw/tests/example.spec.ts
+++ pw/tests/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('get started link', async ({ page }) => {
   await page.goto(process.env.baseURL || "http://localhost:5173");
-
+  console.log("Base URL:", process.env.baseURL);
   await page.locator("div.Tatooine").click();
   await expect(page.locator(".SWCard-selected.Tatooine .title")).toHaveText("Tatooine");
 });
