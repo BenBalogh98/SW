cd83d9e630a5c8c240669f0b97e1dae5f6b6bf27
NIX\bence.balogh <bence.balogh@nixs.com>
2025-11-06 16:25:13 +0100

    cypress spy

--- componentTests/cypress/component/card.cy.tsx
+++ componentTests/cypress/component/card.cy.tsx
@@ -15,7 +15,7 @@ describe("<card /> component", () => {
 
     const onClickFunction = () => { };
 
-    cy.spy(onClickFunction);
+
 
     it("mounts", () => {
         mount(
@@ -33,5 +33,46 @@ describe("<card /> component", () => {
                 key={"title"}
             />
         )
+    })
+
+    it("displays the correct title", () => {
+        mount(
+            <Card
+                isSelected={false}
+                isLeaveButtonVisible={false}
+                onClick={onClickFunction}
+                title={"title"}
+                className={"className"}
+                content={contents}
+                hasLeaveButton={true}
+                leaveButtonIMGContent={"leaveButtonIMGContent"}
+                backgroundImage={"backgroundImage"}
+                onSelect={() => { }}
+                key={"title"}
+            />
+        )
+        cy.get('.title').contains("title");
+    })
+
+    it("calls onClick when clicked", () => {
+        const onClickSpy = cy.spy().as('onClickSpy');
+        mount(
+            <Card
+                isSelected={false}
+                isLeaveButtonVisible={false}
+                onClick={() => { onClickSpy("something"); }}
+                title={"title"}
+                className={"className"}
+                content={contents}
+                hasLeaveButton={true}
+                leaveButtonIMGContent={"leaveButtonIMGContent"}
+                backgroundImage={"backgroundImage"}
+                onSelect={() => { }}
+                key={"title"}
+            />
+        )
+        cy.get('.SWCard').click();
+        cy.get('@onClickSpy').should('have.been.called');
+        cy.get('@onClickSpy').should('have.been.calledWith', 'something');
     })
 })
